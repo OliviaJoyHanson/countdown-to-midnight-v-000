362dcc029bd3012d273a4cf9b4e797ccae219e95
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-04-20 13:20:34 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,9 +8,10 @@ def countdown(number)
 end
 
 def countdown_with_sleep(number)
-  sleep(1.seconds) while number > 0
+  while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
+    sleep.(1)
   end
   "HAPPY NEW YEAR!"
 end
